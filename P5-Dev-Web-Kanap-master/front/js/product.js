d27c86afdad8617003684d916494f7a4cf998fa3
Arnaud <arnaud.trama@gmail.com>
2022-01-04 13:14:29 +0100

    page details

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -9,5 +9,35 @@ fetch(productId)
   .then((response) => response.json())
   .then(function (data) {
     let resultats = data;
-    console.log(resultats.name);
+    printProduct(resultats);
+
+    /*--ajout du produit dans le localStorage--*/
+
+    document.getElementById("addToCart").addEventListener("click", function () {
+      let storage = window.localStorage;
+      storage.setItem("canape", "le contenu de ma variable");
+      alert(storage.getItem("canape"));
+    });
   });
+
+function printProduct(resultats) {
+  let image = document.createElement("img");
+  image.src = resultats.imageUrl;
+  image.alt = resultats.altTxt;
+  document.querySelector(".item__img").appendChild(image);
+
+  let title = document.getElementById("title");
+  let price = document.getElementById("price");
+  title.textContent = resultats.name;
+  price.textContent = resultats.price;
+
+  let description = document.getElementById("description");
+  description.textContent = resultats.description;
+
+  for (let i = 0; i < resultats.colors.length; i++) {
+    let option = document.createElement("option");
+    option.value = resultats.colors[i];
+    option.textContent = resultats.colors[i];
+    document.getElementById("colors").appendChild(option);
+  }
+}
