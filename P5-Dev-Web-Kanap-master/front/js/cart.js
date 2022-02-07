d497e145990ac42b4cb343f034c550222b0a9489
Arnaud <arnaud.trama@gmail.com>
2022-02-07 08:47:07 +0100

    calcul total panier

--- P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -8,8 +8,8 @@ panier.forEach((produit) => {
     .then((response) => response.json())
     .then(function (data) {
       let resultats = data;
-      console.log(resultats);
-      /*--l'article cart__item--*/
+      /*-- console.log(resultats);--*/
+      /*--l'article cart__items--*/
       let articleCartItem = document.createElement("article");
       articleCartItem.setAttribute("class", "cart__item");
       articleCartItem.setAttribute("data-id", produit.id);
@@ -19,8 +19,8 @@ panier.forEach((produit) => {
       let divCartImg = document.createElement("div");
       divCartImg.setAttribute("class", "cart__item__img");
       let cartImg = document.createElement("img");
-      cartImg.setAttribute("img", resultats.img);
-      cartImg.setAttribute("alt", resultats.alt);
+      cartImg.setAttribute("src", resultats.imageUrl);
+      cartImg.setAttribute("alt", resultats.altTxt);
       articleCartItem.appendChild(divCartImg);
       divCartImg.appendChild(cartImg);
       /*--la div cart__item__content--*/
@@ -36,11 +36,11 @@ panier.forEach((produit) => {
       divCartContent.appendChild(divCartDescription);
       /*--le produit--*/
       let cartName = document.createElement("h2");
-      cartName.setAttribute("data-name", produit.name);
+      cartName.textContent = produit.name;
       let cartColor = document.createElement("p");
-      cartColor.setAttribute("data-color", produit.color);
+      cartColor.textContent = produit.colors;
       let cartPrice = document.createElement("p");
-      cartPrice.setAttribute("data-price", produit.price);
+      cartPrice.textContent = resultats.price + " €";
       divCartDescription.appendChild(cartName);
       divCartDescription.appendChild(cartColor);
       divCartDescription.appendChild(cartPrice);
@@ -53,19 +53,59 @@ panier.forEach((produit) => {
         "cart__item__content__settings__quantity"
       );
       let divSettingsQuantityP = document.createElement("p");
-      divSettingsQuantityP.setAttribute("input", "itemQuantity");
+      divSettingsQuantityP.textContent = "Qté : ";
+      let divSettingsQuantityInput = document.createElement("input");
+      divSettingsQuantityInput.setAttribute("class", "itemQuantity");
+      divSettingsQuantityInput.setAttribute("type", "number");
+      divSettingsQuantityInput.setAttribute("name", "itemQuantity");
+      divSettingsQuantityInput.setAttribute("min", "1");
+      divSettingsQuantityInput.setAttribute("max", "100");
+      divSettingsQuantityInput.setAttribute("value", produit.quantity);
       divCartContent.appendChild(divSettings);
       divSettings.appendChild(divSettingsQuantity);
       divSettingsQuantity.appendChild(divSettingsQuantityP);
-
+      divSettingsQuantity.appendChild(divSettingsQuantityInput);
       /*--la div pour supprimer un element--*/
       let divDelete = document.createElement("div");
       divDelete.setAttribute("class", "cart__item__content__settings__delete");
-      let divPDelete = document.createElement("p");
-      divPDelete.setAttribute("class", "deleteItem");
-
-      console.log(section);
+      let divDeleteP = document.createElement("p");
+      divDeleteP.setAttribute("class", "deleteItem");
+      divDeleteP.textContent = "Supprimer";
+      divDelete.appendChild(divDeleteP);
+      divSettings.appendChild(divDelete);
     });
 });
+
+/*--le calcul du panier--*/
+
+async function gettotalPrice() {
+  var priceTotal = 0;
+  var totalQuantity = 0;
+  for (var m = 0; m < panier.length; m++) {
+    await fetch(urlApi + panier[m].id)
+      .then((response) => response.json())
+      .then(function (data) {
+        let resultats = data;
+        console.log(panier[m].quantity);
+        let priceProduct =
+          parseInt(resultats.price) * parseInt(panier[m].quantity);
+
+        /*--variable du prix total du panier--*/
+        priceTotal += priceProduct;
+        let productTotalPrice = document.getElementById("totalPrice");
+        productTotalPrice.innerHTML = priceTotal;
+
+        /*--variable de la quantité total du panier--*/
+        totalQuantity += panier[m].quantity;
+        let productTotalQuantity = document.getElementById("totalQuantity");
+        productTotalQuantity.innerHTML = totalQuantity;
+
+        console.log(panier);
+        console.log(totalQuantity);
+        console.log(priceTotal);
+      });
+  }
+}
+gettotalPrice();
 
 /**envoyer le bon de commande (post) **/
